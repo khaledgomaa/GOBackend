36e5fb04afb0328beb561a14fa2f42743db52a19
khaledgomaa <36606053+khaledgomaa@users.noreply.github.com>
2026-08-28 20:25:13 +0300

    Adding env variables for config

--- Social/cmd/api/main.go
+++ Social/cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/gobackend/social/internal/env"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":4000",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 	app := &application{
 		config: cfg,
--- Social/go.mod
+++ Social/go.mod
@@ -2,7 +2,11 @@ module github.com/gobackend/social
 
 go 1.26.5
 
+require github.com/go-chi/chi v1.5.5
+
 require (
-	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.3.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
+
+tool github.com/joho/godotenv/cmd/godotenv
